6d8ec82af09e823a91bab5bc8877f989fa46cc12
Ninkovski <ninkovski03@gmail.com>
2024-11-26 18:46:25 -0500

    first commit

---
+++ src/main/java/com/nttdata/bootcamp_customer_service/BootcampCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp_customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp_customer_service/BootcampCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp_customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5b8835d3685f6512e756a408dcc5ac8612167732
Ninkovski <ninkovski03@gmail.com>
2024-11-28 20:18:03 -0500

    Implementando reglas de negocio

--- src/main/java/com/nttdata/bootcamp_customer_service/repository/BankProductRepository.java
+++ src/main/java/com/nttdata/bootcamp_customer_service/repository/BankProductRepository.java
@@ -1,16 +1,13 @@
 package com.nttdata.bootcamp_customer_service.repository;
 
 import com.nttdata.bootcamp_customer_service.model.collection.BankProduct;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
-import java.util.List;
-
 @Repository
 public interface BankProductRepository extends ReactiveMongoRepository<BankProduct, String> {
-    // buscar por número de producto si lo necesitamos
-    BankProduct findByProductNumber(String productNumber);
-
+    @Query("{ 'accountHolders.customerId': ?0 }")
     Flux<BankProduct> findAllByCustomerId(String customerId);
 }
